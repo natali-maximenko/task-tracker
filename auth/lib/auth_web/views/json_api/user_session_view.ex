19aa1b5e0e2d8520d2d9c9aac5a0664d0849a8f2
Natalya Maksimenko <natalya.maksimenko@karuna.group>
2022-05-04 14:39:33 +0300

    Step-2: add phx auth

---
+++ auth/lib/auth_web/views/json_api/user_session_view.ex
@@ -0,0 +1,23 @@
+defmodule AuthWeb.JsonApi.UserSessionView do
+  use AuthWeb, :view
+
+  def render("create.json", %{user: user, jwt: jwt}) do
+    %{
+      status: :ok,
+      data: %{
+        token: jwt,
+        email: user.email
+      },
+      message:
+        "You are successfully logged in! Add this token to authorization header to make authorized requests."
+    }
+  end
+
+  def render("error.json", %{message: message}) do
+    %{
+      status: :not_found,
+      data: %{},
+      message: message
+    }
+  end
+end
